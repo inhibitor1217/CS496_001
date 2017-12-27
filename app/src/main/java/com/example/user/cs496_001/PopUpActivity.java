33c2ef42dcdd2bdd806ea0551cf6b3f40cdffa0e
inhibitor1217 <cytops1217@gmail.com>
2017-12-27 22:20:15 +0900

    2020
    - Added "Rotate Image" Button

--- app/src/main/java/com/example/user/cs496_001/PopUpActivity.java
+++ app/src/main/java/com/example/user/cs496_001/PopUpActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -23,7 +26,7 @@ public class PopUpActivity extends AppCompatActivity {
         int position = intent.getExtras().getInt("position");
         ArrayList<Uri> imgList = (ArrayList<Uri>) intent.getExtras().getSerializable("uriList");
 
-        ImageView imageView = (ImageView) findViewById(R.id.popup_imgview);
+        final ImageView imageView = (ImageView) findViewById(R.id.popup_imgview);
 
         Bitmap bitmap = null;
         try {
@@ -33,6 +36,14 @@ public class PopUpActivity extends AppCompatActivity {
         }
 
         imageView.setImageBitmap(bitmap);
+
+        ImageButton rotateButton = (ImageButton) findViewById(R.id.button_imagerotation);
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setRotation(imageView.getRotation() - 90);
+            }
+        });
 
     }
 }
