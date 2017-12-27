87d35e8f2cacbf714aa29bcc941dea22fbdc203e
inhibitor1217 <cytops1217@gmail.com>
2017-12-27 13:22:11 +0900

    1321
     Minor changes on TAB A

--- app/src/main/java/com/example/user/cs496_001/CustomAdapter.java
+++ app/src/main/java/com/example/user/cs496_001/CustomAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class CustomAdapter extends ArrayAdapter<Pair<String, String>> {
     public View getView(int i, View convertView, ViewGroup viewGroup) {
 
         TextView text_name, text_number;
+        ImageView img_person;
 
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_listview, viewGroup, false);
@@ -28,6 +30,7 @@ public class CustomAdapter extends ArrayAdapter<Pair<String, String>> {
 
         text_name = (TextView) convertView.findViewById(R.id.name);
         text_number = (TextView) convertView.findViewById(R.id.number);
+        img_person = (ImageView) convertView.findViewById(R.id.img);
 
         Pair<String, String> obj = getItem(i);
         text_name.setText(obj.first);
